8e44293d99c1bc3881654a18f19feff274633632
JbyteSecurity <jordandiaz2016@gmail.com>
2023-09-19 19:56:33 -0500

    Se agrego encriptacion

--- src/pages/PaymentMethods/PaymentCard.jsx
+++ src/pages/PaymentMethods/PaymentCard.jsx
@@ -53,6 +53,13 @@ class PaymentMethods extends Component {
       Culqi.settings({
         currency: currency,
         amount: amount,
+        xculqirsaid: 'de35e120-e297-4b96-97ef-10a43423ddec',
+        rsapublickey: `-----BEGIN PUBLIC KEY-----
+        MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDswQycch0x/7GZ0oFojkWCYv+g
+        r5CyfBKXc3Izq+btIEMCrkDrIsz4Lnl5E3FSD7/htFn1oE84SaDKl5DgbNoev3pM
+        C7MDDgdCFrHODOp7aXwjG8NaiCbiymyBglXyEN28hLvgHpvZmAn6KFo0lMGuKnz8
+        HiuTfpBl6HpD6+02SQIDAQAB
+        -----END PUBLIC KEY-----`,        
       });
       setTimeout(() => {
         this.setState({
